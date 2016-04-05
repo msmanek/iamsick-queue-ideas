94340a874c66b2fdac7064f8e461be7629351a92
MohammedShabbar Manek <shabbar_manek@live.com>
2016-04-05 11:36:07 -0400

    finished 3 ideas

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$(".enqueued-patient").children("p").hide();
+	$(".enqueued-patient").mouseenter(function(){
+		$(this).css("border", "4px solid blue");
+	}).mouseleave(function() {
+    	$(this).css("border", "solid 2px");
+	}).click(function(){
+		$(this).children("p").toggle();
+	});
+});
