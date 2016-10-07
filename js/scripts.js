5de9a9b3982685efaeb2ba191d099705be1ebfeb
Shabbar <shabbar_manek@live.com>
2016-10-07 11:30:54 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$(".enqueued-patient").mouseenter(function(){
 		$(this).css("border", "4px solid blue");
 	}).mouseleave(function() {
-    	$(this).css("border", "solid 2px");
+    		$(this).css("border", "solid 2px");
 	}).click(function(){
 		$(this).children("p").toggle();
 	});
